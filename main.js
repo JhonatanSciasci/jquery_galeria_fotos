019dd36018472fadf7e3b639d3d582f842125449
Jhonatan <jhon.sciasci@gmail.com>
2024-06-14 12:15:53 -0300

    Initial commit

---
+++ main.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('header button').click(function() {
+        $('form').slideDown();
+    })
+
+    $('#botao-cancelar').click(function() {
+        $('form').slideUp();
+    })
+
+    $('form').on('submit', function(e) {
+        e.preventDefault();
+        const EnderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+        const NovoItem = $('<li style="display: none"></li>');
+        $(`<img src="${EnderecoDaNovaImagem}" />`).appendTo(NovoItem);
+        $(`
+            <div class="overlay-image-link">
+            <a href="${EnderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">
+                Ver imagem em tamanho real
+            </a>
+            </div>
+        `).appendTo(NovoItem);
+        $(NovoItem).appendTo('ul');
+        $(NovoItem).fadeIn(1000);
+        $('#endereco-imagem-nova').val('')
+    })
+})
